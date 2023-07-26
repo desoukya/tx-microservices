a8bbd9ad2780f54599ea7269e74f422110084462
Amr Desouky <desoukya@gmail.com>
2023-07-26 16:07:33 -0500

    update middleware to not run for introspection queries

--- shared/middleware/src/context.middleware.ts
+++ shared/middleware/src/context.middleware.ts
@@ -2,8 +2,6 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 // import * as axios from 'axios';
 import { NextFunction, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
-// import { ExtendedRequest } from '';
-// import { i18NextService } from '';
 import { AppContext } from 'tx-shared-interfaces';
 
 @Injectable()
@@ -11,10 +9,8 @@ export class ContextMiddleware implements NestMiddleware {
   constructor() {}
   async use(req: AppContext, _res: Response, next: NextFunction) {
     if (req?.body?.operationName === 'IntrospectionQuery') {
-      console.log('Introspection Query');
       return next();
     }
-    console.log('Inside ContextMiddleware!', req.body);
     // Assign a unique id to this request for logging purposes
     req.id = uuidv4();
     // Assign unleash to request
@@ -23,9 +19,6 @@ export class ContextMiddleware implements NestMiddleware {
     req.connectors = {
       // postgres: axios.default,
     };
-    // Assign translations to request
-    // req.t = language ? this.i18NService.getFixedT(language) : this.i18NService.getT();
-
     // Proceed with request
     next();
   }
